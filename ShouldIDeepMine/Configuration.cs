8eb6084c31153dc995ca4a2bdb18e71b8fa0c37c
JustPixelll <pixelgod56@gmail.com>
2026-08-25 03:16:06 +0200

    Add smart scan and stale-data settings

--- ShouldIDeepMine/Configuration.cs
+++ ShouldIDeepMine/Configuration.cs
@@ -5,11 +5,15 @@ namespace ShouldIDeepMine;
 [Serializable]
 public sealed class Configuration : IPluginConfiguration
 {
-    public int Version { get; set; } = 1;
-    public int RequestSpacingMs { get; set; } = 2200;
+    public int Version { get; set; } = 2;
+    public int RequestSpacingMs { get; set; } = 2500;
     public int RequestTimeoutMs { get; set; } = 12000;
-    public int MaxRetries { get; set; } = 3;
+    public int MaxRetries { get; set; } = 2;
     public int CategoryItemLimit { get; set; } = 500;
+    public int SmartQueryBudget { get; set; } = 75;
+    public int SmartNativeFreshMinutes { get; set; } = 90;
+    public int StaleAgeHours { get; set; } = 24;
+    public List<DeepMineUpdateList> UpdateLists { get; set; } = new();
 
     public void Save() => Plugin.PluginInterface.SavePluginConfig(this);
 }
